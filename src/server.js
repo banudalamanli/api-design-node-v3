c5498ed656dff2f4e71a8e634a736fb17edb0e40
banudalamanli <banudalamanli@gmail.com>
2020-05-01 10:58:12 +0300

    routes exercises

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,21 +13,37 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-app.get('/', (req, res) => {
-  res.send({ message: "Hello" })
+const log = (req, res, next) => {
+  console.log('logging')
+  next()
+}
+
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello'})
 })
 
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.send({ message: "ok"})
+// Cats
+const routes = ['get /cat', 'get /cat/:id', 'post /cat', 'put /cat/:id', 'delete /cat/:id']
+
+app.use('/api', router)
+
+// CRUD
+//Read
+app.get('/data', [log, log, log], (req, res) => {
+  res.send({ data: [1, 2, 3] })
 })
 
-app.get('/data', (req, res) => {
-  res.send({
-    message: "Hello"
-  })
+//Update
+app.put('/data', (req, res) => {
+
 })
 
+// Delete
+app.delete('/data', (req, res) => {
+
+})
+
+//Create
 app.post('/data', (req, res) => {
   console.log(req.body)
   res.send(req.body)
