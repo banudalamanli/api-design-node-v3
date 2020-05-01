9ac1c891528de689feb235ed4b70f8241b178761
banudalamanli <banudalamanli@gmail.com>
2020-05-01 13:26:21 +0300

    Add model

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,17 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 50
+  },
+  status: {
+    default: "active"
+    type: String,
+    required: true,
+    enum: ["active", "complete", "pastdue"]
+  }
+}, { timestamps: true })
 export const Item = mongoose.model('item', itemSchema)
